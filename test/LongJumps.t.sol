0b89ff5541d02c2335c5d7cfc65c46e0cf107318
z80 <z80@ophy.xyz>
2025-02-27 23:41:22 -0500

    this executes as expected on evm.codes
    seems to be a bug in foundry that causes the stack underflow. tbd

---
+++ test/LongJumps.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {PuffDeployer} from "../src/Deployers.sol";
+
+// These interfaces define callable functions on our test contracts
+interface ISaysHello {
+    function sayHello() external view returns (bytes32);
+}
+
+contract LongJumpsTest is Test {
+    PuffDeployer public puffDeployer;
+
+    function setUp() public {
+        puffDeployer = new PuffDeployer();
+    }
+
+    function test_longJump() public {
+        address newContract = puffDeployer.deployContract("examples/long_jump.huff");
+        ISaysHello jumper = ISaysHello(newContract);
+        bytes32 message = jumper.sayHello();
+        
+        // The expected output is "Hello, World!" right-aligned in a bytes32
+
+        assertEq(message, 0x0000000000000000000000000000000000000048656c6c6f2c20576f726c6421);
+    }
+
+}
